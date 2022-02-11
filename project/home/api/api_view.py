145332f5215f0e0877e5629e2529bfed5be0ed8b
askarjon111 <askarjon.abdullayev@gmail.com>
2022-02-11 15:58:22 +0500

    serizalizer fix

--- project/home/api/api_view.py
+++ project/home/api/api_view.py
@@ -596,7 +596,7 @@ def course_detail(request):
         course_id = request.query_params.get("course_id", False)
         if course_id:
             course = Course.objects.get(id=course_id)
-            video_instead_trailer = VideoCourse.objecst.get(courseModule__course=course)
+            video_instead_trailer = VideoCourse.objects.get(courseModule__course=course)
             ser = CourseDetailSerializer(course)
             if ser.data['trailer'] is None:
                 ser.data['trailer'] = video_instead_trailer.video
