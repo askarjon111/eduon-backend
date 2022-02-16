a10e6f3362592fdb3ff274f2262339d48d839d1f
askarjon111 <askarjon.abdullayev@gmail.com>
2022-02-16 15:32:42 +0500

    refresh

--- project/eduon/settings.py
+++ project/eduon/settings.py
@@ -182,8 +182,8 @@ REST_FRAMEWORK = {
         ],
 }
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=200),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
