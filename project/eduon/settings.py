6e53c3e3d0481dc45ecedfb54b12bbfaaad29638
askarjon111 <askarjon.abdullayev@gmail.com>
2022-02-23 14:05:54 +0500

    rating

--- project/eduon/settings.py
+++ project/eduon/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'quiz',
     'rest_framework.authtoken',
     'django_filters',
-    'background_task',
 ]
 
 MIDDLEWARE = [
@@ -91,24 +90,7 @@ CLICK_SETTINGS = {
     'merchant_user_id': '17661'
 }
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'OPTIONS': {
-#                 'read_default_file': '/var/www/eduon_backend/project/mysql.cnf',
-#             },
-#         }
-#     }
-
-if DEBUG:
-    DATABASES = {
-        'default': {
-            "ENGINE": 'django.db.backends.sqlite3',
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-        }
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'OPTIONS': {
@@ -116,6 +98,23 @@ else:
             },
         }
     }
+
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             "ENGINE": 'django.db.backends.sqlite3',
+#             "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'OPTIONS': {
+#                 'read_default_file': '/var/www/eduon_backend/project/mysql.cnf',
+#             },
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -179,9 +178,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': 
         [
             'django_filters.rest_framework.DjangoFilterBackend',
+            # 'django_filters.rest_framework.OrderingFilter',
         ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 7
 }
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=200),
