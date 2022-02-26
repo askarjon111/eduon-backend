41bdca51c91e05520591fe7e21a738868a8204c3
askarjon111 <askarjon.abdullayev@gmail.com>
2022-02-26 07:03:32 +0000

    fixes

--- project/home/tasks.py
+++ project/home/tasks.py
@@ -4,25 +4,25 @@ from home.models import Course, Discount
 from django.db.models import Q
 
 
-@background(schedule=70)
+@background(schedule=600)
 def delete_discount():
     discounts = Discount.objects.filter(expire_day=date.today())
     for discount in discounts:
         course = discount.course
-        course.discount = 0
+        course.discount = 0git
         course.save()
         discount.delete()
 
 
-@background(schedule=60)
+@background(schedule=640800)
 def set_discount_to_random_courses():
-    courses = Course.objects.filter(Q(discount=0) | Q(discount=None))
+    courses = Course.objects.filter(Q(discount=0) | Q(is_confirmed=True))
     courses = courses.order_by('?')[:10]
     for course in courses:
         amount = course.price * 0.5
         expire_day = date.today() + timedelta(days=7)
         Discount.objects.create(
-            course=course, amount=amount, expire_day=date.today())
+            course=course, amount=amount, expire_day=date.expire_day)
         course.discount = amount
         course.save()
 
