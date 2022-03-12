96851aa2e041bfaea1591f26b70873c7a2e6aa56
askarjon111 <askarjon.abdullayev@gmail.com>
2022-03-12 16:22:14 +0500

    tasks

--- project/home/release_task.py
+++ project/home/release_task.py
@@ -6,13 +6,23 @@ from django.db.models import Q
 
 @background(schedule=60)
 def release_speaker():
-    speaker = Speaker.objects.filter(Q(status=0) & Q(
+    speakers = Speaker.objects.filter(Q(status=0) & Q(
         date_of_release=date.today()))
-    speaker.update(status=1, reason_of_ban='')
+    for speaker in speakers:
+        speaker.status = 1
+        speaker.reason_of_ban = ''
+        speaker.date_of_release=None
+        speaker.save()
+    # speaker.update(status=1, reason_of_ban='', date_of_release='')
     
 
 @background(schedule=60)
 def release_user():
-    user = Users.objects.filter(Q(is_active=False) & Q(
+    users = Users.objects.filter(Q(is_active=False) & Q(
         date_of_release=date.today()))
-    user.update(is_active=True, reason_of_ban='')
+    for user in users:
+        user.is_active = True
+        user.reason_of_ban = ''
+        user.date_of_release = None
+        user.save()
+    # user.update(is_active=True, reason_of_ban='')
