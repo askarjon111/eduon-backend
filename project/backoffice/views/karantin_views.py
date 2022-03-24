597ba3ac60b12d3e51f56c1ff962303b2e1ac188
askarjon111 <askarjon.abdullayev@gmail.com>
2022-03-24 08:04:44 +0000

    conf

--- project/backoffice/views/karantin_views.py
+++ project/backoffice/views/karantin_views.py
@@ -19,56 +19,51 @@ def karantin(request):
     paginator = PageNumberPagination()
     if query == 'speakers':
         speakers = Speaker.objects.filter(status=0)
-        paginator.page_size = 12
-        page = paginator.paginate_queryset(speakers, request)
-        serializer = SpeakerSerializer(speakers, many=True, context={'request': request})
+        # speakers_page = paginator.paginate_queryset(speakers, request)
+        speakers = SpeakerSerializer(speakers, many=True)
         data = {
-            "speakers": serializer.data,
+            "speakers": speakers.data,
         }
 
 
     elif query == 'courses':
         courses = Course.objects.filter(is_banned=True).order_by('-id')
-        paginator.page_size = 12
-        page = paginator.paginate_queryset(courses, request)
-        serializer = CourseListSerializer(
-            page, many=True, context={'request': request})
+        # courses_page = paginator.paginate_queryset(courses, request)
+        courses = CourseListSerializer(
+            courses, many=True)
 
         data = {
-            "courses": serializer.data,
+            "courses": courses.data,
         }
 
 
     elif query == 'users':
-        users = Users.objects.filter(is_active=False, date_of_release=None).order_by('-id')
-        paginator.page_size = 12
-        page = paginator.paginate_queryset(users, request)
-        serializer = UserSerializer(
-            page, many=True, context={'request': request})
+        users = Users.objects.filter(
+            is_active=False, date_of_release=None).order_by('-id')
+        # users_page = paginator.paginate_queryset(users, request)
+        users = UserSerializer(
+            users, many=True)
 
         data = {
-            "users": serializer.data,
+            "users": users.data,
         }
 
 
     elif query == 'all':
-        paginator.page_size = 12
-
         speakers = Speaker.objects.filter(status=0)
-        speakers_page = paginator.paginate_queryset(speakers, request)
-        speakers = SpeakerSerializer(
-            speakers_page, many=True, context={'request': request})
+        # speakers_page = paginator.paginate_queryset(speakers, request)
+        speakers = SpeakerSerializer(speakers, many=True)
 
         users = Users.objects.filter(
             is_active=False, date_of_release=None).order_by('-id')
-        users_page = paginator.paginate_queryset(users, request)
+        # users_page = paginator.paginate_queryset(users, request)
         users = UserSerializer(
-            users_page, many=True, context={'request': request})
+            users, many=True)
 
         courses = Course.objects.filter(is_banned=True).order_by('-id')
-        courses_page = paginator.paginate_queryset(courses, request)
+        # courses_page = paginator.paginate_queryset(courses, request)
         courses = CourseListSerializer(
-            courses_page, many=True, context={'request': request})
+            courses, many=True)
 
         data = {
             "speakers": speakers.data,
@@ -81,4 +76,4 @@ def karantin(request):
         }
         return Response(data)
 
-    return paginator.get_paginated_response(data)
+    return Response(data)
